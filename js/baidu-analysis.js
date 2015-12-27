361231bade41dc2a36d72c133f1c4efbabdb3fd4
loongeek <wizardof21cn@163.com>
2015-12-27 10:37:22 +0800

    Site updated: 2015-12-27 10:37:22

---
+++ js/baidu-analysis.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?c6f31c2a181feddaaff58d4befce579b";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
---
+++ js/baidu_analytics.js
@@ -0,0 +1,13 @@
+<% if (theme.baidu_analytics){ %>
+<!-- 百度统计 -->
+<script type="text/javascript">
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?c6f31c2a181feddaaff58d4befce579b";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
+<!-- End 百度统计 -->
+<% } %>
